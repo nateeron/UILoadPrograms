dd980b1446f7f13c28bb87d5647b323abc71fcee
cayoshi <nateeronron@gmail.com>
2024-06-10 13:28:04 +0700

    ADI

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+
+
+import './App.css'
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [time, setTime] = useState('');
+  const [date, setDate] = useState('');
+
+  useEffect(() => {
+    function startTime() {
+      const today = new Date();
+      const d = today.getDate();
+      const mo = today.getMonth() + 1;
+      const y = today.getFullYear() + 543;
+      const monn = checday(mo);
+      let h = today.getHours();
+      let m = today.getMinutes();
+      let s = today.getSeconds();
+      m = checkTime(m);
+      s = checkTime(s);
+      setTime(`${h}:${m}:${s}`);
+      setDate(`${d} ${monn} ${y}`);
+      setTimeout(startTime, 500);
+    }
+
+    startTime();
+
+    return () => clearTimeout(startTime); // Cleanup timeout on unmount
+  }, []);
+
+  function checkTime(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
+
+  function checday(mon) {
+    let cout = "";
+    switch (mon) {
+      case 1: cout = "มกราคม"; break;
+      case 2: cout = "กุมภาพันธ์"; break;
+      case 3: cout = "มีนาคม"; break;
+      case 4: cout = "เมษายน"; break;
+      case 5: cout = "พฤษภาคม"; break;
+      case 6: cout = "มิถุนายน"; break;
+      case 7: cout = "กรกฎาคม"; break;
+      case 8: cout = "สิงหาคม"; break;
+      case 9: cout = "กันยายน"; break;
+      case 10: cout = "ตุลาคม"; break;
+      case 11: cout = "พฤศจิกายน"; break;
+      case 12: cout = "ธันวาคม"; break;
+      default: cout = "";
+    }
+    return cout;
+  }
+
+  return (
+    <div className='con' >
+    <div >
+        <div className="p" align="center">
+          <font className="c sss" id="txt">{time}</font>
+        </div>
+        <div className="p2" align="center">
+          <font className="c2 sss" id="txt2">{date}</font>
+        </div>
+    </div> </div>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <div style={{ backgroundColor: "#161616", color: "#fff" }}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+        ,
+    </div>
+);
